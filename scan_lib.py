8d8995addfa88ec40afa9917564d3ac839c51159
Mykola <0xb000@gmail.com>
2020-02-20 21:41:51 +0200

    Implement result store and todays book chosing

--- scan_lib.py
+++ scan_lib.py
@@ -2,14 +2,22 @@
 
 book_costs = []
 
+def interleave(items, num):
+  res = [[]]*num
+  for k, it in enumerate(items):
+    res[k % num].append(it)
+
+  return res
+
 class BookLib:
-  def __init__(self, init_books, signup_delay, scans_per_day):
+  def __init__(self, ind, init_books, signup_delay, scans_per_day):
     self.books = init_books
     self.signup_delay = signup_delay
     self.scans_per_day = scans_per_day
 
     self.signup_day = None
     self.sent_books = []
+    self.ind = ind
 
   def cost(self, days_left):
     # @todo
@@ -26,10 +34,13 @@ class BookLib:
     if (self.signup_day is None) || (self.signup_day + self.signup_delay < day_num):
       return []
     else:
-      raise "todo"
+      # @todo: use max heap
+      chosen_books = sorted(self.books, key=lambda b: book_costs[b], reverse=True)[:self.scans_per_day]
+      self.sent_books.extend(chosen_books)
+      return chosen_books
 
   def getSentBooks(self):
-    pass
+    return self.sent_books
 
   def signup(self, day):
     self.signup_day = day
@@ -63,7 +74,10 @@ class ScanQuest:
       libs.remove(best_lib)
       signed_libs.append(best_lib)
 
-      for today in range(day_num, day_num+best_lib.signup_delay)
+      for today in range(day_num, day_num+best_lib.signup_delay):
+        if today >= self.days_total:
+          break
+
         for lib in self.signed_libs:
           books = lib.sendSomeBooks(today)
           self.burnSomeBooks(books)
@@ -71,7 +85,12 @@ class ScanQuest:
       day_num += best_lib.signup_delay
 
   def writeResult(self, fname):
-    # @todo
+    with open(fname, "w") as f:
+      f.write("{}\n".format(len(self.signed_libs)))
+      for lib in self.signed_libs:
+        books = lib.getSentBooks()
+        f.write("{} {}\n".format(lib.ind, len(books)))
+        f.write(" ".join(map(str, books))+"\n")
 
 def main():
   q = ScanQuest("data.in")
